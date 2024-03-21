85529abc60247a1c3545877c8127f758f12b8db0
Nico P <nicopetkas@gmail.com>
2024-03-21 15:16:11 -0700

    server update

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # employee-tracker
+
+## Description
+
+
+
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Technologies](#technologies)
+- [Contributing](#contributing)
+- [Demo](#demo)
+- [Questions](#questions)
+
+
+
+## Installation
+
+1. Clone the repository `git clone git@github.com:NPetkas/employee-tracker.git`.
+2. Navigate to the project directory in terminal.
+3. Install dependencies `npm i`.
+4. Run `mysql -u root -p` to start MYSQL.
+5. inside mysql, source schema`SOURCE db/schema.sql` and seeds `SOURCE db/seeds.sql`.
+6. `Exit` mysql.
+
+
+
+## Usage
+
+1. Start app in terminal by running `npm start`.
+2. Select from the employee tracker options list for desired action(s)
+3. When finished, exit server by typing `control(^) + 'c'`.
+
+
+
+## Technologies
+
+- Inquirer
+- MYSQL
+
+
+
+## Contributing
+
+To contribute:
+
+1. Create a new branch
+2. Make your changes
+3. Submit a pull request
+
+
+
+## Demo
+
+![image](Assets/note-taker-demo.gif)
+
+[Employee Tracker Demo][def]
+
+[def]: 
+
+
+
+
+## Questions
+
+Contact information for any questions:
+
+Github Profile: [https://github.com/NPetkas](https://github.com/NPetkas)
+
+Link to Repo: [https://github.com/NPetkas/employee-tracker](https://github.com/NPetkas/employee-tracker)
+
+Email: [nicopetkas@gmail.com](nicopetkas@gmail.com)
--- server.js
+++ server.js
@@ -1,172 +1,201 @@
-const inquirer = require('inquirer');
-const connection = require('./db/connection');
+const connection = require("./db/connection");
+const inquirer = require("inquirer");
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log('connected to database');
-    startTracker();
-})
+  if (err) throw err;
+  console.log("Connected Successfully!");
+
+  startTracker();
+});
+
+function init() {
+  console.log(`
+
+ __________________________________________________________________________
+|                                                                          |
+|    _____           _                     _____                           | 
+|   |   __|_____ ___| |___ _ _ ___ ___    |     |___ ___ ___ ___ ___ ___   |
+|   |   __|     | . | | . | | | -_| -_|   | | | | .'|   | .'| . | -_|  _|  |
+|   |_____|_|_|_|  _|_|___|_  |___|___|   |_|_|_|__,|_|_|__,|_  |___|_|    |
+|               |_|       |___|                             |___|          |
+|                                                                          |
+|__________________________________________________________________________|                
+                
+                
+                `);
+}
 
 function startTracker() {
-    inquirer.prompt([ 
-        {
-            type: 'list',
-            name: 'option',
-            message: 'Select from the following:',
-            choices: [
-                'View all departments',
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add an employee',
-                'Update an employee role'
-                ]
-        }
-    ]).then(answer => {
-        switch (answer.option) {
-            case 'View all departments':
-                viewDepartment();
-                break;
-            case 'View all roles':
-                viewRoles();
-                break;
-            case 'View all employees':
-                viewEmployees();
-                break;
-            case 'Add a department':
-                addDepartment();
-                break; 
-            case 'Add a role':
-                addRole();
-                break;
-            case 'Add an employee':
-                addEmployee();
-                break;
-            case 'Update an employee role':
-                updateEmployeeRole();
-                break;
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "option",
+        message: "Select from the following:",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.option) {
+        case "View all departments":
+          viewDepartment();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateEmployeeRole();
+          break;
 
-            default:
-                console.log('select an option');
-                break;
-        }
+        default:
+          console.log("select an option");
+          break;
+      }
     });
 }
 
 function viewDepartment() {
-    connection.query('SELECT * FROM department', (err, res) => {
-        if (err) throw error;
-        console.table(res);
-        startTracker();
-    });
+  connection.query("SELECT * FROM department", (err, results) => {
+    if (err) throw err;
+    console.table(results);
+    startTracker();
+  });
 }
 
 function viewRoles() {
-    connection.query('SELECT * FROM roles', (err, res) => {
-        if (err) throw error;
-        console.table(res);
-        startTracker();
-    });
+  connection.query("SELECT * FROM roles", (err, results) => {
+    if (err) throw err;
+    console.table(results);
+    startTracker();
+  });
 }
 
 function viewEmployees() {
-    connection.query('SELECT * FROM employees', (err, res) => {
-        if (err) throw error;
-        console.table(res);
-        startTracker();
-    });
+  connection.query("SELECT * FROM employees", (err, results) => {
+    if (err) throw err;
+    console.table(results);
+    startTracker();
+  });
 }
 
 function addDepartment() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Enter department name:'
-        }
-    ]).then(answer => {
-        connection.query('INSERT INTO department SET ?', answer, (err, res) => {
-            if (err) throw error;
-            console.log('Added Successfully!');
-            startTracker();
-        });
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter department name:",
+      },
+    ])
+    .then((answer) => {
+      connection.query("INSERT INTO department SET ?", answer, (err, res) => {
+        if (err) throw err;
+        console.log("Added Successfully!");
+        startTracker();
+      });
     });
 }
 
 function addRole() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'Enter role:'
-        },
-        {
-            type: 'input',
-            name: 'salary',
-            message: 'Enter salary:'
-        },
-        {
-            type: 'input',
-            name: 'departmentId',
-            message: 'Enter department id:'
-        }
-    ]).then(answer => {
-        connection.query('INSERT INTO role SET ?', answer, (err, res) => {
-            if (err) throw error;
-            console.log('Added Successfully!');
-            startTracker();
-        });
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "Enter role:",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Enter salary:",
+      },
+      {
+        type: "input",
+        name: "department_id",
+        message: "Enter department id:",
+      },
+    ])
+    .then((answer) => {
+      connection.query("INSERT INTO roles SET ?", answer, (err, res) => {
+        if (err) throw err;
+        console.log("Added Successfully!");
+        startTracker();
+      });
     });
 }
 
 function addEmployee() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'firstName',
-            message: 'Employee first name:'
-        },
-        {
-            type: 'input',
-            name: 'lastName',
-            message: 'Employee last name:'
-        },
-        {
-            type: 'input',
-            name: 'roleId',
-            message: 'Employee role id:'
-        }
-    ]).then(answer => {
-        connection.query('INSERT INTO employees SET ?', answer, (err, res) => {
-            if (err) throw error;
-            console.log('Added Successfully!');
-            startTracker();
-        });
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "Employee first name:",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "Employee last name:",
+      },
+      {
+        type: "input",
+        name: "roles_id",
+        message: "Employee role id:",
+      },
+    ])
+    .then((answer) => {
+      connection.query("INSERT INTO employees SET ?", answer, (err, res) => {
+        if (err) throw err;
+        console.log("Added Successfully!");
+        startTracker();
+      });
     });
 }
 
 function updateEmployeeRole() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'employeeId',
-            message: 'Enter Employee id:'
-        },
-        {
-            type: 'input',
-            name: 'roleId',
-            message: 'Enter role id:'
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "employee_id",
+        message: "Enter Employee id:",
+      },
+      {
+        type: "input",
+        name: "roles_id",
+        message: "Enter role id:",
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "UPDATE employees SET roles_id = ? WHERE id =?",
+        [answer.roles_id, answer.employee_id], (err, res) => {
+          if (err) throw err;
+          console.log("Updated Successfully!");
+          startTracker();
         }
-    ]).then(answer => {
-        connection.query('UPDATE employees SET roleId = ? WHERE id =?', 
-        [answer.roleId, answer.employeeId],
-         (err, res) => {
-            if (err) throw error;
-            console.log('Updated Successfully!');
-            startTracker();
-        });
+      );
     });
 }
 
-
+init();
